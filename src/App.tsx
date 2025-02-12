0bc33bbfa5750227eac4a947788ec5a52a50280f
Prabodhani Karunarathne <karunarathneprabodhani@gmail.com>
2025-02-12 10:24:51 +0530

    completed home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { ThemeProvider, createTheme, CssBaseline } from '@mui/material';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import TopBar from "./components/topBar/topBar";
+import NavigationBar from "./components/navigationBar/navigationBar";
+import HomePage from "./pages/homePage/homePage";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const theme = createTheme({
+    typography: {
+      fontFamily: `'Poppins', sans-serif`,
+    },
+    palette: {
+      mode: 'light', // Ensures light mode
+      text: {
+        primary: 'rgba(3, 3, 3, 0.87)',
+      },
+      background: {
+        default: '#ffffff',
+      },
+    },
+  });
+  
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <ThemeProvider theme={theme}>
+      <TopBar />
+      
+      <Router>
+      <NavigationBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/services" element={<ServicesPage />} />
+        <Route path="/company" element={<CompanyPage />} />
+        <Route path="/contact-us" element={<ContactUsPage />} />
+        <Route path="/careers" element={<CareersPage />} />
+        <Route path="/other-services" element={<OtherServicesPage />} /> */}
+      </Routes>
+    </Router>
+    </ThemeProvider>
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+
+
