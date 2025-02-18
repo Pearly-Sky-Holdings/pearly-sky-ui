b29c3955abdbbff9591d555dcb8e681c5af5b57f
Mishan20 <ishansenanayaka1@gmail.com>
2025-02-18 22:33:17 +0530

    Edit regual basic page

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ import UiContactUsPage from './pages/HomeContactUsPage/uiContactUsPage.tsx';
 import ContactUsPage from './pages/contactUsPage/contactUsPage';
 import OurLocations from "./pages/ourLocationPage/ourLocationPage.tsx";
 import CleanServices from './pages/cleanServices/cleanServices';
-import RegularBasicCleaning from './pages/services/regularbasicCleaningPage.tsx';
+import RegularBasicCleaning from './pages/servicesPages/regularbasicCleaningPage.tsx';
 
 function App() {
 
