2b38ac8b27998e14f27a6ef31265501208b3af7c
PiyumalNipuna60 <nipuna315np@gmail.com>
2025-03-05 00:53:35 +0530

    update

--- src/components/bookingSectionCarts/bookingSectionCart.tsx
+++ src/components/bookingSectionCarts/bookingSectionCart.tsx
@@ -652,10 +652,12 @@ const BookingSectionCart: React.FC<BookingSectionCartProps> = ({
           placeholder="Auto-suggested / Hours : minutes"
         />
         {/* Add the message below the dropdown */}
-        <div className="text-sm text-white  bg-blue-400 px-1">
-        <InfoIcon  sx={{ color: 'red', mx:1 }} />
+        {propertySize && (
+        <div className="text-sm text-blue-500   px-1">
+        <InfoIcon  sx={{ color: 'black', mx:1 }} />
           Depending on your space's conditions, the cleaning duration will be decreased or extended.
         </div>
+        )}
       </div>
 
       {/* Custom Input for Number of Cleaners */}
